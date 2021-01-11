1fbae2eaeace7f9fd210436d59ed96cb3c7522b2
Aditya Prawira <adityaprawira96@gmail.com>
2021-01-12 10:33:34 +1100

    Adding error handlers for ticketing-app

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -14,6 +18,25 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+app.all("*", () => {
+    throw new NotFoundError();
 });
+
+app.use(errorHandler);
+
+const startDatabase = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+    } catch (err) {
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log("Listening on port 3000");
+    });
+};
+
+startDatabase();
