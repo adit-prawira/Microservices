359b462b1c1882c3bd994c11aff3250c17db21d4
Aditya Prawira <adityaprawira96@gmail.com>
2021-01-14 09:33:56 +1100

    Added custom hooks and functionalities to sign in and sign up

---
+++ ticketing-app/client/pages/_app.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+const _App = ({ Component, pageProps }) => {
+    return (
+        <div>
+            <h1>BITCH</h1>
+            <Component {...pageProps} />
+        </div>
+    );
+};
+export default _App;
