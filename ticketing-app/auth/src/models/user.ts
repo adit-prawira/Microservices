bdcd9396dfe4e018c2b192b433577ae97a2aec25
Aditya Prawira <adityaprawira96@gmail.com>
2021-01-13 19:45:24 +1100

    Adding cookies and JWT

---
+++ ticketing-app/auth/src/models/user.ts
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+// Interface that describes properties required to create new User
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+// Interface that describes properties that User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// Interface that describes properties that User document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.password;
+                delete ret.__v;
+            },
+        },
+    }
+);
+
+// Hash the stored password
+userSchema.pre("save", async function (done) {
+    if (this.isModified("password")) {
+        const hashed = await Password.toHash(this.get("password"));
+        this.set("password", hashed);
+    }
+
+    done();
+});
+
+// Make Typescript to make sure if attributes are valid
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
--- ticketing-app/auth/src/routes/signup.ts
+++ ticketing-app/auth/src/routes/signup.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -14,15 +17,39 @@ router.post(
             .isLength({ min: 4, max: 20 })
             .withMessage("Password must be between 4 and 20 characters"),
     ],
-    (req: Request, res: Response) => {
-        const errors = validationResult(req);
+    validateRequest,
+    async (req: Request, res: Response) => {
+        //parse request data
+        const { email, password } = req.body;
 
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
+        // Check if registered user data already exist
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            throw new BadRequestError("Email in use");
         }
 
-        throw new DatabaseConnectionError();
-        // res.send({});
+        // Create a new user and save them to MongoDB
+        const user = User.build({
+            email,
+            password,
+        });
+        await user.save();
+
+        // Generate JWT
+        const userJwt = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+            },
+            process.env.JWT_SECRET!
+        );
+
+        //Store in session object
+        req.session = {
+            jwt: userJwt,
+        };
+
+        res.status(201).send(user);
     }
 );
 
