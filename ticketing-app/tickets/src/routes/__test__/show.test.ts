bfb6d38323fe03c7ae9ab46f26c2147e53de80d9
Aditya Prawira <adityaprawira96@gmail.com>
2021-01-15 11:20:50 +1100

    Update @apticketz/common and fix error handling functionality

--- ticketing-app/tickets/src/routes/__test__/show.test.ts
+++ ticketing-app/tickets/src/routes/__test__/show.test.ts
@@ -1,10 +1,10 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
 it("returns 404 if the ticket is not found", async () => {
-    const res = await request(app).get("/api/tickets/asdfghjkl").send();
-
-    console.log(res.body);
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it("returns the ticket if the ticket is found", async () => {
