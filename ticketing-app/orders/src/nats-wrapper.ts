40774fa8d0ba42c1bc999cf4394338f3c06d7749
Aditya Prawira <adityaprawira96@gmail.com>
2021-01-19 16:08:35 +1100

    Added orders service deplyment files, setups and building docker image

---
+++ ticketing-app/orders/src/nats-wrapper.ts
@@ -0,0 +1,29 @@
+import nats, { Stan } from "node-nats-streaming";
+
+class NatsWrapper {
+    // This property might be undefined for some period of time
+    private _client?: Stan;
+
+    get client() {
+        if (!this._client) {
+            throw new Error("Cannot access NATS client before connecting");
+        }
+        return this._client;
+    }
+    connect(clusterId: string, clientId: string, url: string) {
+        this._client = nats.connect(clusterId, clientId, { url });
+
+        return new Promise<void>((resolve, reject) => {
+            // this.client is referencing to get client()
+            this.client.on("connect", () => {
+                console.log("Connected to NATS");
+                resolve();
+            });
+            this.client.on("error", (err) => {
+                reject(err);
+            });
+        });
+    }
+}
+
+export const natsWrapper = new NatsWrapper();
