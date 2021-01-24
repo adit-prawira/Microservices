5b9ae744961df8cac774656d1bcc3ac7fab53396
Aditya Prawira <adityaprawira96@gmail.com>
2021-01-24 18:30:50 +1100

    Added UI to create, purchase, see tickets/orders, and pay orders

--- ticketing-app/client/pages/tickets/new.js
+++ ticketing-app/client/pages/tickets/new.js
@@ -1,11 +1,32 @@
 import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
 const NewTicket = () => {
     const [title, setTitle] = useState("");
     const [price, setPrice] = useState("");
+
+    const { doRequest, errors } = useRequest({
+        url: "/api.tickets",
+        method: "post",
+        body: { title, price },
+        onSuccess: () => Router.push("/"),
+    });
+    const onSubmit = (event) => {
+        event.preventDefault();
+        doRequest();
+    };
+    const onBlur = () => {
+        const value = parseFloat(price);
+        if (isNaN()) {
+            return;
+        }
+        setPrice(value.toFixed(2));
+    };
+
     return (
         <div>
             <h1>Create a ticket</h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-group">
                     <label>Title</label>
                     <input
@@ -18,10 +39,12 @@ const NewTicket = () => {
                     <label>Price</label>
                     <input
                         value={price}
+                        onBlur={onBlur}
                         onChange={(e) => setPrice(e.target.value)}
                         className="form-control"
                     />
                 </div>
+                {errors}
                 <button className="btn btn-primary">Submit</button>
             </form>
         </div>
